4c57ce35cc069543651e44a71b5595c0c90f7acc
matcalvani <matstraty@gmail.com>
2023-02-27 14:43:04 -0800

    css

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
   <footer>
     <div class="copyright">
-      <p class="copyright-text"> Copyright <span> &#169 </span> 2022 Cafe' Dolce.</p>
+      <p class="copyright-text"> Copyright <span> &#169 </span> 2023 Cafe' Dolce.</p>
     </div>
   </footer>
   <?php 
